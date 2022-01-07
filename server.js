7f4148dda92234b54f20b99d26d66971482e787c
JavaKaran <sharmakaran12092001@gmail.com>
2022-01-07 21:00:48 +0530

    commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+})
+.then(() => {
+  console.log('Database Connected!!');
+})
+.catch((err) => {
+  console.log(err);
+})
+
+app.use(express.json({ extended: false }));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/boards', require('./routes/api/boards'));
+app.use('/api/lists', require('./routes/api/lists'));
+app.use('/api/cards', require('./routes/api/cards'));
+app.use('/api/checklists', require('./routes/api/checklists'));
+
+// // Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('Server started on port ' + PORT));
